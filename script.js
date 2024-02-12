92b0d8ad8a0548ab6e683746e3a708f5f19c70ea
Winktory1304 <thomaswinkler88@googlemail.com>
2024-02-12 17:50:48 +0100

    first pokemon

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let currentPokemon;
+
+
 async function loadPokemon(){
 let url ='https://pokeapi.co/api/v2/pokemon/charmander';
+let response = await fetch(url);
+currentPokemon = await response.json();
+
+renderPokemonInfo();
+console.log('Loaded pokemon', currentPokemon);
+}
+
+function renderPokemonInfo(){
+  document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+  document.getElementById('pokemonPicture').src = currentPokemon['sprites']['front_shiny'];
+
 }
