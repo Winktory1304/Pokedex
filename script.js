e11af448ba148666074ad1249d8fa2c6997e44d0
Winktory1304 <thomaswinkler88@googlemail.com>
2024-02-29 10:27:50 +0100

    symbols for types

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ let morePokemonToLoad =20;
 let currentPokemon;
 let allPokemon = [];
 let pokemonTypes = {
-    'normal': '#A8A878',
-    'fire': '#F08030',
-    'water': '#6890F0',
-    'electric': '#F8D030',
-    'grass': '#78C850',
-    'ice': '#98D8D8',
-    'fighting': '#C03028',
-    'poison': '#A040A0',
-    'ground': '#E0C068',
-    'flying': '#a890f0',
-    'psychic': '#f85888',
-    'bug': '#a8b820',
-    'rock': '#b8a038',
-    'ghost': '#705898',
-    'dragon': '#7038f8',
-    'dark': '#705949',
-    'steel': '#b8b8d0',
-    'fairy': '#f0b6bc',
-    'stellar': '#f0b6bc'
+    'normal': { 'color': '#A8A878', 'img': '/img/normal.png' },
+    'fire': { 'color': '#F08030', 'img': '/img/fire.png' },
+    'water': { 'color': '#6890F0', 'img': '/img/water.png' },
+    'electric': { 'color': '#F8D030', 'img':'/img/electric.png' },
+    'grass': { 'color': '#78C850', 'img': '/img/grass.png' },
+    'ice': { 'color': '#98D8D8', 'img': '/img/ice.png' },
+    'fighting': { 'color': '#C03028', 'img': '/img/fighting.png' },
+    'poison': { 'color': '#A040A0', 'img': '/img/poison.png' },
+    'ground': { 'color': '#E0C068', 'img': '/img/ground.png' },
+    'flying': { 'color': '#a890f0', 'img': '/img/flying.png' },
+    'psychic': { 'color': '#f85888', 'img': '/img/psychic.png' },
+    'bug': { 'color': '#a8b820', 'img': '/img/bug.png' },
+    'rock': { 'color': '#b8a038', 'img': '/img/rock.png' },
+    'ghost': { 'color': '#705898', 'img': '/img/ghost.png' },
+    'dragon': { 'color': '#7038f8', 'img': '/img/dragon.png' },
+    'dark': { 'color': '#705949', 'img': '/img/dark.png' },
+    'steel': { 'color': '#b8b8d0', 'img': '/img/steel.png' },
+    'fairy': { 'color': '#f0b6bc', 'img': '/img/fairy.png' },
+    'stellar': { 'color': '#f0b6bc', 'img': '/img/stellar.png' }
 };
+
 
 
 async function init() {
@@ -50,9 +51,17 @@ function render(i) {
     let renderTypesAmount = currentPokemon['types'];
     let renderPokemonType1 = currentPokemon['types']['0']['type']['name'];
     let renderPokemonType2 = '';
+    let type2Img = '';
     //Überprüfen, ob renderPokemonType2 einen Typ besitzt
     if (renderTypesAmount.length > 1) {
         renderPokemonType2 = currentPokemon['types']['1']['type']['name']
+    }
+    // Überprüfe, ob renderPokemonType2 einen Wert hat
+    if (renderPokemonType2) {
+        // Wenn renderPokemonType2 einen "truthy" Wert hat, erstelle das Bild-Tag für den zweiten Typ
+        type2Img = `<img class="typeLogo mt-16px p-border" alt="${renderPokemonType2}" src='${pokemonTypes[renderPokemonType2].img}'></img>`
+    } else {
+        type2Img = '';
     }
     // Img wird mit Daten Attributen versheen die in der der renderDetailView wieder aufgerufen werden
     document.getElementById('pokemonContent').innerHTML +=
@@ -62,15 +71,15 @@ function render(i) {
                         <img id="pokekomPicture${i}" class="pokemonPicture" src="${currentPokemon['sprites']['other']['dream_world']['front_default']}" onclick="renderDetailView(${i})">      
                     </div>
                     <div class="info-container">
-                    <div class="types">
-                        <p class="mt-16px p-border">${renderPokemonType1}</p>
-                        <p>${renderPokemonType2}</p>
+                    <div class="types">                        
+                        <img class="typeLogo mt-16px p-border" alt="${renderPokemonType1}" src='${pokemonTypes[renderPokemonType1].img}'>
+                        ${type2Img}                       
                     </div>
                     <div class="number-container">#${renderPokemonIndex}</div>
                     <div class="more-information">Click Pokemon for more informations</div>
                 </div>`;
     // Hintergrundfarbe wird nach Type angepasst
-    document.getElementById(`pokedex${i}`).style.backgroundColor = pokemonTypes[renderPokemonType1];
+    document.getElementById(`pokedex${i}`).style.backgroundColor = pokemonTypes[renderPokemonType1].color;
 }
 
 function load20More(){
